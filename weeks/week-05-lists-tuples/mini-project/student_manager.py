7ad23f6ea2385c80cae1b7d07cd57d425bae0cbe
NguyenNhuDuc <letienhub@gmail.com>
2026-09-23 16:29:16 +0700

    upd code dự án

--- weeks/week-05-lists-tuples/mini-project/student_manager.py
+++ weeks/week-05-lists-tuples/mini-project/student_manager.py
@@ -11,51 +11,59 @@ while True:
 
     lua_chon = input("Vui lòng chọn chức năng (0-4): ")
 
-    if lua_chon == '1':
+    if lua_chon == "1":
         print("Bạn đã chọn thêm sinh viên")
-        masv = input("Nhập mã số sinh viên: ")
-        hoten = input("Nhập họ tên sinh viên: ")
-        tuoi = int(input("Nhập tuổi: "))
-        ngay_sinh = input("Nhập ngày sinh: ")
-        gioi_tinh = input("Nhập giới tính: ")
-        sinh_vien = [masv, hoten, tuoi, ngay_sinh, gioi_tinh]
-        danh_sach.append(sinh_vien)
-        print("Bạn đã thêm sinh viên thành công")
+        so_luong = int(input("Nhập sô lượng sinh viên cần thêm: "))
+        if so_luong <= 0:
+            print("Số lượng phải lớn hơn 0")
+        for i in range(so_luong):
+            print("Nhập thông tin sinh viên thứ ", i + 1, ":\n")
+            masv = input("Nhập mã số sinh viên: ")
+            hoten = input("Nhập họ tên sinh viên: ")
+            tuoi = int(input("Nhập tuổi: "))
+            ngay_sinh = input("Nhập ngày sinh: ")
+            gioi_tinh = input("Nhập giới tính: ")
+            sinh_vien = [masv, hoten, tuoi, ngay_sinh, gioi_tinh]
+            danh_sach.append(sinh_vien)
+            print("Bạn đã thêm sinh viên thành công")
 
-    elif lua_chon == '2':
+    elif lua_chon == "2":
         print("Bạn đã chọn xem danh sách")
-        print("Danh sách: ", danh_sach)
+        for i in range(len(danh_sach)):
+            print("Sinh viên thứ", i + 1, ":\n")
+            sv = danh_sach[i]
+            print(f"Mã sv: {sv[0]}\nHọ tên: {sv[1]}\nTuổi: {sv[2]}\nNgày sinh: {sv[3]}\nGiới tính: {sv[4]}\n")
 
-    elif lua_chon == '3':
+    elif lua_chon == "3":
         print("Bạn đã chọn cập nhật thông tin")
         masv = input("Nhập mã số sinh viên cần cập nhật: ")
-        for sinh_vien in danh_sach:
-            if sinh_vien[0] == masv:
-                print("Nhập thông tin mới")
+        for i in range(len(danh_sach)):
+            if danh_sach[i][0] == masv:
+                print("Bạn đã tìm thấy sinh viên")
+                print("Nhập thông tin mới:\n")
+                masv = input("Nhập mã số sinh viên: ")
                 hoten = input("Nhập họ tên sinh viên: ")
                 tuoi = int(input("Nhập tuổi: "))
                 ngay_sinh = input("Nhập ngày sinh: ")
                 gioi_tinh = input("Nhập giới tính: ")
-                sinh_vien[1] = hoten
-                sinh_vien[2] = tuoi
-                sinh_vien[3] = ngay_sinh
-                sinh_vien[4] = gioi_tinh
-                print("Bạn đã cập nhật thông tin thành công")
-                break
-        else:
-            print("Không tìm thấy sinh viên với mã số", masv)
-
-    elif lua_chon == '4':
+                danh_sach[i] = [masv, hoten, tuoi, ngay_sinh, gioi_tinh]
+                print("Bạn đã cập nhật thông tin sinh viên thành công")
+                break  
+            else:
+                print("Không tìm thấy sinh viên") 
+                
+    elif lua_chon == "4":
         print("Bạn đã chọn xóa sinh viên")
         masv = input("Nhập mã số sinh viên cần xóa: ")
-        for sinh_vien in danh_sach:
-            if sinh_vien[0] == masv:
-                danh_sach.remove(sinh_vien)
+        for i in range(len(danh_sach)):
+            if danh_sach[i][0] == masv:
+                danh_sach.remove(danh_sach[i])
                 print("Bạn đã xóa sinh viên thành công")
-                break
-        else:
-            print("Không tìm thấy sinh viên với mã số", masv)
+                break       
 
-    elif lua_chon == '0':
+    elif lua_chon == "0":
         print("Kết thúc chương trình")
         break
+    else:
+        print("Lựa chọn không hợp lệ")
+
